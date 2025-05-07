07ea56ba33af83f540394bf35f3d8713f6b52d2c
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-07 15:38:11 +0545

    feat:Create Map using reactleaflet

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './map.scss'
+import "leaflet/dist/leaflet.css";
+import Pin from '../pin/Pin';
 
-const Map = () => {
+function Map({data}){
+  console.log( data)
   return (
-    <div>
-      Map
-    </div>
+   
+    <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {data.map(item=>(
+      <Pin item={item} key={item.id}/>
+    ))}
+  </MapContainer>
   )
 }
 
