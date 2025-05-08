f6f054aced0753fb34f92e111f233bc00cca332d
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-08 19:43:54 +0545

    feat:Create common axios

---
+++ client/src/lib/apiRequest.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiRequest = axios.create({
+  baseURL: 'http://localhost:8800/api',
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true, // for cookies
+  },
+});
+
+export default apiRequest;
