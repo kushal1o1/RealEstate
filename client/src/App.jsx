ed9a8b69a0b364e72ac4ca3d6444bc064b712fb0
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-08 17:01:03 +0545

    fix:provide route for llogin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ListPage from './routes/listPage/ListPage'
 import Layout from './routes/layout/Layout'
 import SinglePage from './routes/singlePage/SinglePage'
 import ProfilePage from './routes/profilePage/ProfilePage'
+import Login from './routes/login/Login'
 
 function App() {
   const router = createBrowserRouter([
@@ -26,6 +27,10 @@ function App() {
         {
           path: '/profile',
           element: <ProfilePage />,
+        },
+        {
+          path:"/login",
+          element:<Login/>
         },
       ]
     }
