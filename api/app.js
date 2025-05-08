2c377d041de2d9f5c51f59b58b89cd7c70dc587a
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-08 20:17:49 +0545

    test:check RBAC and authorization

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
+import testRoute from './routes/test.route.js';
 
 const app = express();
 app.use(cors({
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/test', testRoute);
 
 app.listen(8800, () => {
   console.log('Server is running on port 8800');
