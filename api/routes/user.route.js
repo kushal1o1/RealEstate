054502126ae4ee6f7598cb9ccf5cc2e169501de6
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-11 20:10:37 +0545

    feat:create route for profile posts saved and list

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import { getUser, updateUser, deleteUser, getUsers,savePost } from '../controllers/user.controller.js';
+import { getUser, updateUser, deleteUser, getUsers,savePost,profilePosts } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 
 const router = express.Router();
 
 router.get('/',getUsers);
-router.get('/:id',verifyToken,getUser);
+// router.get('/:id',verifyToken,getUser); //only for development 
 router.put('/:id',verifyToken,updateUser);
 router.delete('/:id',verifyToken,deleteUser);
 router.post('/save',verifyToken,savePost);
+router.get('/profilePosts',verifyToken,profilePosts);
+
 
 export default router;
