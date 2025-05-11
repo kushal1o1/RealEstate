e8f7eb55faae1fb9c70a7e540670d90d5d0b8302
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-11 20:11:46 +0545

    feat:dom loaderfor profile posts saved and  own list

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -13,14 +13,12 @@ export const singlePageLoader = async ({ request,params }) => {
 
 export const listPageLoader = async ({ request,params }) => {
 const query =request.url.split('?')[1];//provide 2 part as array before after '?'
-console.log(query);
   const res =  apiRequest.get('/posts?'+query);
   return {postResponse :res,};
-  // try {
-  //   const res = await apiRequest.get('/posts/'+ params.id);
-  //   return res.data;
-  // } catch (error) {
-  //   console.error("Error fetching post:", error);
-  //   throw new Error("Failed to load post data");
-  // }
+}
+
+export const profilePageLoader = async () => {
+
+  const res =  apiRequest.get('/users/profilePosts');
+  return {postResponse :res,};
 }
