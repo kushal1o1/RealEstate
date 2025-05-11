477bfae19b7c42561ee9c2442958d83730f41686
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-11 14:45:47 +0545

    feat:create domloader for list

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -9,3 +9,17 @@ export const singlePageLoader = async ({ request,params }) => {
     throw new Error("Failed to load post data");
   }
 }
+
+
+export const listPageLoader = async ({ request,params }) => {
+const query =request.url.split('?')[1];//provide 2 part as array before after '?'
+  const res = await apiRequest.get('/posts?'+query);
+  return res.data;
+  // try {
+  //   const res = await apiRequest.get('/posts/'+ params.id);
+  //   return res.data;
+  // } catch (error) {
+  //   console.error("Error fetching post:", error);
+  //   throw new Error("Failed to load post data");
+  // }
+}
