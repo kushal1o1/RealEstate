a0d5bd88b24437d832230ae96ee625991416c242
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-11 20:10:58 +0545

    feat:create controller for profile posts saved and list

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -128,3 +128,35 @@ export const savePost = async (req, res) => {
     res.status(500).json({ message: "Failed to get that post!" });
   }
 };
+
+
+
+export const profilePosts = async (req, res) => {
+    const tokenUserId = req.params.userId;
+    try {
+        const userPosts = await prisma.post.findMany({
+            where: {
+                userId: tokenUserId,
+            },
+        });
+
+         const saved = await prisma.savedPost.findMany({
+            where: {
+                userId: tokenUserId,
+                
+            },
+            include:{
+                  post:true,
+
+                },
+        });
+        const savedPosts = saved.map((item)=>item.post)
+        res.status(200).json({
+            userPosts,
+            savedPosts
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Failed to get profile posts" });
+    }
+}
