7e12c0bff68763582998a8fee1f5d010e1758907
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-11 19:30:17 +0545

    feat:implemented Ui for save and handlesubmit

--- client/src/routes/singlePage/SinglePage.jsx
+++ client/src/routes/singlePage/SinglePage.jsx
@@ -2,12 +2,32 @@ import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
 import { singlePostData, userData } from "../../lib/dummydata";
-import { useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData } from "react-router-dom";
 import DOMPurify from "dompurify";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import apiRequest from "../../lib/apiRequest";
 
 function SinglePage() {
   const post = useLoaderData();
-  console.log(post);
+  const [saved, setSaved] = useState(post.isSaved);
+  const {currentUser} = useContext(AuthContext); 
+  // console.log(post);
+  const handleSave = async () => {
+    // TODO:optimistics hook use ok later
+    setSaved(!saved);
+    try{
+      if(!currentUser){
+        redirect('/login');
+      }
+      await apiRequest.post('/users/save',{postId:post.id});
+
+    }catch(err){
+      console.log(err);
+    setSaved(!saved);
+
+    }
+  }
   
   return (
     <div className="singlePage">
@@ -122,9 +142,9 @@ function SinglePage() {
               <img src="/chat.png" alt="" />
               Send a Message
             </button>
-            <button>
+            <button onClick={handleSave} style={{backgroundColor:saved ? '#fece51' : 'white'}}>
               <img src="/save.png" alt="" />
-              Save the Place
+            {saved ? 'Place Saved':' Save the Place'} 
             </button>
           </div>
         </div>
