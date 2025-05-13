aa20862827c837c7843dd2de59e6b83fcc6e33c8
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-13 15:00:51 +0545

    feat:create  notification store

---
+++ client/src/lib/notificationStore.js
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+
+export const useNotificationStore = create((set) => ({
+  number: 0,
+  fetch: async () => {
+    const res = await apiRequest("/users/notification")
+    set({number:res.data })
+  },
+  decrease : () => set((state) => ({ number: state.number - 1 })),
+  reset: () => set({ number: 0 }),
+ 
+}))
