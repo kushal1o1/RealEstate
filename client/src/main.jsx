21e13d8ef1e4c86ca3b69ece09ba1f8a0a7ab968
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-23 20:23:15 +0545

    feat:wrapp toastprovider in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import "./index.scss"
 import { AuthContextProvider } from './context/AuthContext.jsx'
 import { SocketContextProvider } from './context/SocketContext.jsx'
+import { ToastProvider } from './context/ToastContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-       <SocketContextProvider>
-        <App />
-       </SocketContextProvider>
-    </AuthContextProvider>
+    <ToastProvider>
+      <AuthContextProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
+      </AuthContextProvider>
+    </ToastProvider>
   </React.StrictMode>
  
 )
