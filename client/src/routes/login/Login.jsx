ebc91373d041ab0a785d751a4a819280d39975cf
Kushal Baral <136962554+kushal1o1@users.noreply.github.com>
2025-05-27 20:17:11 +0545

    fix:toast style and login redirect if admin

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,5 +1,5 @@
 import "./login.scss";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import apiRequest from "../../lib/apiRequest";
 import { AuthContext } from "../../context/AuthContext";
@@ -7,15 +7,12 @@ import { useContext } from "react";
 import { useToast } from "../../context/ToastContext";
 import Loader from "../../components/loader/Loader";
 
-
 function Login() {
-  const [error,setError] = useState('');
-  const [isLoading,setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const { showToast } = useToast();
-  
-
-    const {updateUser} = useContext(AuthContext);
+  const { updateUser } = useContext(AuthContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,20 +22,23 @@ function Login() {
     const username = formData.get("username");
     const password = formData.get("password");
 
-    setIsLoading(true);
-
     try {
-      const res = await apiRequest.post('/auth/login', 
-      {
-         username,
-         password }
-      );
+      const res = await apiRequest.post('/auth/login', {
+        username,
+        password
+      });
+      
       updateUser(res.data);
       showToast("Login Successful", 'success');
+      
+      // Redirect based on admin status
+      if (res.data.isAdmin) {
+        navigate('/admin-dashboard');
+      } else {
         navigate('/');
+      }
     } catch (err) {
       console.error(err);
-      // setError('An error occurred. Please try again later.');
       showToast("Invalid Credentials", 'error');
     } finally {
       setIsLoading(false);
@@ -50,9 +50,20 @@ function Login() {
       <div className="formContainer">
         <form onSubmit={handleSubmit}> 
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" required
-          minLength={3} maxLength={20} />
-          <input name="password" type="password" placeholder="Password" />
+          <input 
+            name="username" 
+            type="text" 
+            placeholder="Username" 
+            required
+            minLength={3} 
+            maxLength={20} 
+          />
+          <input 
+            name="password" 
+            type="password" 
+            placeholder="Password" 
+            required
+          />
           <button disabled={isLoading}>Login</button>
           {error && <span className="error">{error}</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
